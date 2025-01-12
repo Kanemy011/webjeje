2cfa87b8d7e4dd7befd4f80b2bb7937d50d14d64
anaïs <165287307+ikwaru@users.noreply.github.com>
2025-01-12 19:12:44 +0100

    Update vue.config.js
    carpeta correcta en vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js file to be place in the root of your repository
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?
-  '/practica2/' : '/' //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
+  '/webjeje/' : '/' //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
   }
   
